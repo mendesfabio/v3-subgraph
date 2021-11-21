42aae018f7d84694f296d4aa3729cbdff6f0d71e
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-11-21 10:05:13 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Uniswap V3 Subgraph
+
---
+++ src/mappings/core.ts
@@ -0,0 +1,254 @@
+/* eslint-disable prefer-const */
+import { Bundle, Pool, Token } from '../types/schema'
+import { Pool as PoolABI } from '../types/Factory/Pool'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import {
+  Burn as BurnEvent,
+  Flash as FlashEvent,
+  Initialize,
+  Mint as MintEvent,
+  Swap as SwapEvent
+} from '../types/templates/Pool/Pool'
+import { convertTokenToDecimal, safeDiv } from '../utils'
+import { ONE_BI, ZERO_BD } from '../utils/constants'
+import { findEthPerToken, getEthPriceInUSD, getTrackedAmountUSD, sqrtPriceX96ToTokenPrices } from '../utils/pricing'
+
+export function handleInitialize(event: Initialize): void {
+  let pool = Pool.load(event.address.toHexString())
+  pool.sqrtPrice = event.params.sqrtPriceX96
+  pool.tick = BigInt.fromI32(event.params.tick)
+  // update token prices
+  let token0 = Token.load(pool.token0)
+  let token1 = Token.load(pool.token1)
+
+  // update ETH price now that prices could have changed
+  let bundle = Bundle.load('1')
+  let ethPriceUSD = getEthPriceInUSD()
+  bundle.ethPriceUSD = ethPriceUSD
+  bundle.save()
+
+  // update token prices
+  let token0derivedETH = findEthPerToken(token0 as Token)
+  let token1derivedETH = findEthPerToken(token1 as Token)
+  token0.derivedETH = token0derivedETH
+  token1.derivedETH = token1derivedETH
+  token0.priceUSD = token0derivedETH.times(ethPriceUSD)
+  token1.priceUSD = token1derivedETH.times(ethPriceUSD)
+  token0.save()
+  token1.save()
+}
+
+export function handleMint(event: MintEvent): void {
+  let bundle = Bundle.load('1')
+  let poolAddress = event.address.toHexString()
+  let pool = Pool.load(poolAddress)
+
+  let token0 = Token.load(pool.token0)
+  let token1 = Token.load(pool.token1)
+  let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals)
+  let amount1 = convertTokenToDecimal(event.params.amount1, token1.decimals)
+
+  // update token0 data
+  token0.txCount = token0.txCount.plus(ONE_BI)
+  token0.totalValueLocked = token0.totalValueLocked.plus(amount0)
+  token0.totalValueLockedUSD = token0.totalValueLocked.times(token0.derivedETH.times(bundle.ethPriceUSD))
+
+  // update token1 data
+  token1.txCount = token1.txCount.plus(ONE_BI)
+  token1.totalValueLocked = token1.totalValueLocked.plus(amount1)
+  token1.totalValueLockedUSD = token1.totalValueLocked.times(token1.derivedETH.times(bundle.ethPriceUSD))
+
+  // pool data
+  pool.txCount = pool.txCount.plus(ONE_BI)
+
+  // Pools liquidity tracks the currently active liquidity given pools current tick.
+  // We only want to update it on mint if the new position includes the current tick.
+  if (
+    pool.tick !== null &&
+    BigInt.fromI32(event.params.tickLower).le(pool.tick as BigInt) &&
+    BigInt.fromI32(event.params.tickUpper).gt(pool.tick as BigInt)
+  ) {
+    pool.liquidity = pool.liquidity.plus(event.params.amount)
+  }
+
+  pool.totalValueLockedToken0 = pool.totalValueLockedToken0.plus(amount0)
+  pool.totalValueLockedToken1 = pool.totalValueLockedToken1.plus(amount1)
+  pool.totalValueLockedETH = pool.totalValueLockedToken0
+    .times(token0.derivedETH)
+    .plus(pool.totalValueLockedToken1.times(token1.derivedETH))
+  pool.totalValueLockedUSD = pool.totalValueLockedETH.times(bundle.ethPriceUSD)
+
+  token0.save()
+  token1.save()
+  pool.save()
+}
+
+export function handleBurn(event: BurnEvent): void {
+  let bundle = Bundle.load('1')
+  let poolAddress = event.address.toHexString()
+  let pool = Pool.load(poolAddress)
+
+  let token0 = Token.load(pool.token0)
+  let token1 = Token.load(pool.token1)
+  let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals)
+  let amount1 = convertTokenToDecimal(event.params.amount1, token1.decimals)
+
+  // update token0 data
+  token0.txCount = token0.txCount.plus(ONE_BI)
+  token0.totalValueLocked = token0.totalValueLocked.minus(amount0)
+  token0.totalValueLockedUSD = token0.totalValueLocked.times(token0.derivedETH.times(bundle.ethPriceUSD))
+
+  // update token1 data
+  token1.txCount = token1.txCount.plus(ONE_BI)
+  token1.totalValueLocked = token1.totalValueLocked.minus(amount1)
+  token1.totalValueLockedUSD = token1.totalValueLocked.times(token1.derivedETH.times(bundle.ethPriceUSD))
+
+  // pool data
+  pool.txCount = pool.txCount.plus(ONE_BI)
+  // Pools liquidity tracks the currently active liquidity given pools current tick.
+  // We only want to update it on burn if the position being burnt includes the current tick.
+  if (
+    pool.tick !== null &&
+    BigInt.fromI32(event.params.tickLower).le(pool.tick as BigInt) &&
+    BigInt.fromI32(event.params.tickUpper).gt(pool.tick as BigInt)
+  ) {
+    pool.liquidity = pool.liquidity.minus(event.params.amount)
+  }
+
+  pool.totalValueLockedToken0 = pool.totalValueLockedToken0.minus(amount0)
+  pool.totalValueLockedToken1 = pool.totalValueLockedToken1.minus(amount1)
+  pool.totalValueLockedETH = pool.totalValueLockedToken0
+    .times(token0.derivedETH)
+    .plus(pool.totalValueLockedToken1.times(token1.derivedETH))
+  pool.totalValueLockedUSD = pool.totalValueLockedETH.times(bundle.ethPriceUSD)
+
+
+  token0.save()
+  token1.save()
+  pool.save()
+}
+
+export function handleSwap(event: SwapEvent): void {
+  let bundle = Bundle.load('1')
+  let pool = Pool.load(event.address.toHexString())
+
+  // hot fix for bad pricing
+  if (pool.id == '0x9663f2ca0454accad3e094448ea6f77443880454') {
+    return
+  }
+
+  let token0 = Token.load(pool.token0)
+  let token1 = Token.load(pool.token1)
+
+  // amounts - 0/1 are token deltas: can be positive or negative
+  let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals)
+  let amount1 = convertTokenToDecimal(event.params.amount1, token1.decimals)
+
+  // need absolute amounts for volume
+  let amount0Abs = amount0
+  if (amount0.lt(ZERO_BD)) {
+    amount0Abs = amount0.times(BigDecimal.fromString('-1'))
+  }
+  let amount1Abs = amount1
+  if (amount1.lt(ZERO_BD)) {
+    amount1Abs = amount1.times(BigDecimal.fromString('-1'))
+  }
+
+  let amount0ETH = amount0Abs.times(token0.derivedETH)
+  let amount1ETH = amount1Abs.times(token1.derivedETH)
+  let amount0USD = amount0ETH.times(bundle.ethPriceUSD)
+  let amount1USD = amount1ETH.times(bundle.ethPriceUSD)
+
+  // get amount that should be tracked only - div 2 because cant count both input and output as volume
+  let amountTotalUSDTracked = getTrackedAmountUSD(amount0Abs, token0 as Token, amount1Abs, token1 as Token).div(
+    BigDecimal.fromString('2')
+  )
+  let amountTotalETHTracked = safeDiv(amountTotalUSDTracked, bundle.ethPriceUSD)
+  let amountTotalUSDUntracked = amount0USD.plus(amount1USD).div(BigDecimal.fromString('2'))
+
+  let feesUSD = amountTotalUSDTracked.times(pool.feeTier.toBigDecimal()).div(BigDecimal.fromString('1000000'))
+
+  // pool volume
+  pool.volumeToken0 = pool.volumeToken0.plus(amount0Abs)
+  pool.volumeToken1 = pool.volumeToken1.plus(amount1Abs)
+  pool.volumeUSD = pool.volumeUSD.plus(amountTotalUSDTracked)
+  pool.untrackedVolumeUSD = pool.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
+  pool.feesUSD = pool.feesUSD.plus(feesUSD)
+  pool.txCount = pool.txCount.plus(ONE_BI)
+
+  // Update the pool with the new active liquidity, price, and tick.
+  pool.liquidity = event.params.liquidity
+  pool.tick = BigInt.fromI32(event.params.tick as i32)
+  pool.sqrtPrice = event.params.sqrtPriceX96
+  pool.totalValueLockedToken0 = pool.totalValueLockedToken0.plus(amount0)
+  pool.totalValueLockedToken1 = pool.totalValueLockedToken1.plus(amount1)
+
+  // update token0 data
+  token0.volume = token0.volume.plus(amount0Abs)
+  token0.totalValueLocked = token0.totalValueLocked.plus(amount0)
+  token0.volumeUSD = token0.volumeUSD.plus(amountTotalUSDTracked)
+  token0.untrackedVolumeUSD = token0.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
+  token0.feesUSD = token0.feesUSD.plus(feesUSD)
+  token0.txCount = token0.txCount.plus(ONE_BI)
+
+  // update token1 data
+  token1.volume = token1.volume.plus(amount1Abs)
+  token1.totalValueLocked = token1.totalValueLocked.plus(amount1)
+  token1.volumeUSD = token1.volumeUSD.plus(amountTotalUSDTracked)
+  token1.untrackedVolumeUSD = token1.untrackedVolumeUSD.plus(amountTotalUSDUntracked)
+  token1.feesUSD = token1.feesUSD.plus(feesUSD)
+  token1.txCount = token1.txCount.plus(ONE_BI)
+
+  // updated pool ratess
+  let prices = sqrtPriceX96ToTokenPrices(pool.sqrtPrice, token0 as Token, token1 as Token)
+  pool.token0Price = prices[0]
+  pool.token1Price = prices[1]
+  pool.save()
+
+  // update USD pricing
+  let ethPriceUSD = getEthPriceInUSD()
+  bundle.ethPriceUSD = ethPriceUSD
+  bundle.save()
+
+  token0.derivedETH = findEthPerToken(token0 as Token)
+  token1.derivedETH = findEthPerToken(token1 as Token)
+  let token0derivedETH = findEthPerToken(token0 as Token)
+  let token1derivedETH = findEthPerToken(token1 as Token)
+  token0.derivedETH = token0derivedETH
+  token1.derivedETH = token1derivedETH
+  token0.priceUSD = token0derivedETH.times(ethPriceUSD)
+  token1.priceUSD = token1derivedETH.times(ethPriceUSD)
+
+  /**
+   * Things afffected by new USD rates
+   */
+  pool.totalValueLockedETH = pool.totalValueLockedToken0
+    .times(token0.derivedETH)
+    .plus(pool.totalValueLockedToken1.times(token1.derivedETH))
+  pool.totalValueLockedUSD = pool.totalValueLockedETH.times(bundle.ethPriceUSD)
+
+  token0.totalValueLockedUSD = token0.totalValueLocked.times(token0.derivedETH).times(bundle.ethPriceUSD)
+  token1.totalValueLockedUSD = token1.totalValueLocked.times(token1.derivedETH).times(bundle.ethPriceUSD)
+
+  // update fee growth
+  let poolContract = PoolABI.bind(event.address)
+  let feeGrowthGlobal0X128 = poolContract.feeGrowthGlobal0X128()
+  let feeGrowthGlobal1X128 = poolContract.feeGrowthGlobal1X128()
+  pool.feeGrowthGlobal0X128 = feeGrowthGlobal0X128 as BigInt
+  pool.feeGrowthGlobal1X128 = feeGrowthGlobal1X128 as BigInt
+
+  pool.save()
+  token0.save()
+  token1.save()
+}
+
+export function handleFlash(event: FlashEvent): void {
+  // update fee growth
+  let pool = Pool.load(event.address.toHexString())
+  let poolContract = PoolABI.bind(event.address)
+  let feeGrowthGlobal0X128 = poolContract.feeGrowthGlobal0X128()
+  let feeGrowthGlobal1X128 = poolContract.feeGrowthGlobal1X128()
+  pool.feeGrowthGlobal0X128 = feeGrowthGlobal0X128 as BigInt
+  pool.feeGrowthGlobal1X128 = feeGrowthGlobal1X128 as BigInt
+  pool.save()
+}
